d26e87bba9e4463a4829d2831105f21b9f531f94
Unknown <volzstefan97@googlemail.com>
2018-10-16 19:48:36 +0200

    Added new argument and error message

--- BarcodeFromVideo.py
+++ BarcodeFromVideo.py
@@ -10,10 +10,13 @@ from pyzbar import pyzbar
 
 parser = ArgumentParser(description="Get barcode from video feed")
 parser.add_argument("-c", "--camera_id", dest="camera_id", default=0, type=int, help="ID for the video feed (default: use standart camera)")
-parser.add_argument("-da", "--disable_abort", dest="disable_abort", default=False, type=bool, help="Disable closing the videofeed with esc or lmb (WARNING: Has to be killed if no code is found) (default: don't abort it)")
+parser.add_argument("-da", "--disable_abort", dest="disable_abort", action="store_true", help="Disable closing the videofeed with esc or lmb (WARNING: Has to be killed if no code is found) (default: abort by key/button)")
+parser.add_argument("-m", "--mirror", dest="mirror", action="store_true", help="Invert the video feed along y-axis")
 args = parser.parse_args()
 camera_id = args.camera_id
 disable_abort = args.disable_abort
+mirror = args.mirror
+
 def zbar_rect_correction(x,y,w,h):
     return ((x, y), (x + w, y + h))
 
@@ -38,15 +41,19 @@ window = "SV Barcode Reader"
 cv2.namedWindow(window)
 if not disable_abort:
     cv2.setMouseCallback(window, abort)
+    
 with Camera(camera_id) as camera:
     if camera.isOpened():
         rval, frame = camera.read()
     else:
+        stderr.write("Unable to open Camera")
         rval = False
 
     found_codes = []
     counter = []
     while rval & run:
+        if mirror:
+          frame = cv2.flip(frame,1)
         if cv2.getWindowProperty(window, 0) < 0:
             run = False
         barcodes = pyzbar.decode(frame)
@@ -64,10 +71,7 @@ with Camera(camera_id) as camera:
             x, y = barcode.rect[:2]
             cv2.putText(frame, "{}({})".format(*barcode_information), (x, y-10), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 1)
         cv2.imshow(window, frame)
-        """
-        if barcodes:
-            sleep(5)
-        """
+        
         rval, frame = camera.read()
         key = cv2.waitKey(1)
         if not disable_abort:
