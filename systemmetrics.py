a4e9c9c24b8eab253c6fe529dbf2b1fcb206cd5c
SV-97 <volzstefan97@googlemail.com>
2018-11-02 17:43:26 +0100

    Added to do and docstring

--- systemmetrics.py
+++ systemmetrics.py
@@ -1,9 +1,12 @@
+"""
+ToDo:
+    - Implement remaining SystemMetrics
+"""
 import win32api
 import win32con
 
 class SystemMetrics():
+    """Wrapper Class for win32api.GetSystemMetrics
+    """
     screen_width = win32api.GetSystemMetrics(win32con.SM_CXVIRTUALSCREEN)
     screen_height = win32api.GetSystemMetrics(win32con.SM_CYVIRTUALSCREEN)
-
-if __name__=="__main__":
-    print(f"{SystemMetrics.screen_width}x{SystemMetrics.screen_height}")
